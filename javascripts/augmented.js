8a53f801e6ad1bdcd8d9b8e3c2e5567281d481f9
Bryan Hankins <bryanhankins@me.com>
2016-08-21 09:32:48 -0500

    added select elements

--- javascripts/augmented.js
+++ javascripts/augmented.js
@@ -2,11 +2,15 @@
 var SandwichMaker = (function(maker) {
 
   // Private variable to store the different meat prices
-  var meatPrices;
+  var meatPrices = {};
 
   // Augment the original object with another method
-  maker.addMeat = function() {
-    return ???;
+  maker.addMeat = function(added) {
+    var options = {
+      turkey: 0.90,
+      bacon: 0.45
+    }
+    return SandwichMaker.meatPrices
   };
 
   // Return the new, augmented object with the new method on it
